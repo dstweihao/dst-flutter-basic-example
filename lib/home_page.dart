b216cf2f716d2423fdd35a328850b4353b808d1b
weihao <1120575939@qq.com>
2020-08-02 23:09:26 +0800

    ListView动态赋值、item显示分割线

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic_example/example/listview_page.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -6,6 +7,25 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title:Text("首页")
       ),
+      body: new Column(
+        children: <Widget>[
+          RaisedButton(
+            color: Colors.brown,
+            child: Text("ListView"),
+            onPressed: (){
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context){
+                  return ListViewPage();
+
+                }
+              ));
+
+            },
+
+          )
+
+        ],
+      )
     );
   }
 }
---
+++ lib/modules/listview_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+class ListViewItem  {
+  final String name;
+  final int age;
+  final String country;
+
+  ListViewItem({this.name, this.age, this.country}):super();
+
+}
+
