9a7db3dc2de1612048b39e6b3c908a4d30bdb95f
weihao <1120575939@qq.com>
2020-08-02 22:06:22 +0800

    init project

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basic_example/Home_page.dart';
+
+void main(){
+  runApp(new MyApp());
+
+}
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        title: "Flutter_Exampler",
+        theme: ThemeData(
+          primaryColor: Colors.amber,
+        ),
+        home: HomePage(),
+
+      ),
+    );
+  }
+}
+
